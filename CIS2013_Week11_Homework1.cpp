8b43865859e67a4a3b373f45d8db004156ab470b
kevin115533 <35547586+kevin115533@users.noreply.github.com>
2018-04-09 14:22:03 -0500

    week 11 homework
    added basics

--- CIS2013_Week11_Homework1.cpp
+++ CIS2013_Week11_Homework1.cpp
@@ -1,46 +1,117 @@
 #include <iostream>
-#include <cstring>
+#include <string>
 using namespace std;
 
-class car {
+class carAtts {
+private:
+	string make;
+	string year;
+	string model;
+	string color;
+	bool carOn = false;
 public:
-	char make[20];
-	char year[4];
-	char model[20];
-	char color[20];
-	
-	void selectAtt(char x){
-		if (x == 'y') {
-			cout << "What year is your car? ";
-			cin.getline(year,4);
+	void setMake(string x) {
+		make = x;
+	}
+
+	void setYear(string x) {
+		year = x;
+	}
+	void setModel(string x) {
+		model = x;
+	}
+
+	void setColor(string x) {
+		color = x;
+	}
+
+	void startCar() {
+		if (carOn == false) {
+			cout << "You started your car" << endl;
+			carOn = true;
 		}
-		else if (x == 'm') {
-			cin.getline(make, 20);
-		}
-		else if (x == 'o') {
-			cin.getline(model, 20);
-		}
-		else if(x == 'c') {
-			cin.getline(make, 20);
+		else if (carOn == true) {
+			cout << "Your car is already started" << endl;
 		}
 	}
 
+	void stopCar() {
+		if (carOn == true) {
+			cout << "You stopped your car" << endl;
+			carOn = false;
+		}
+		else if (carOn == false) {
+			cout << "Your car is already off" << endl;
+		}
+	}
 
+	void printCar() {
+		cout << year << make << model << endl;
+	}
 };
 
+
+
 int main() {
-	char stay;
-	char option;
+	bool keepGoing = true;
+	int option;
+	string uMake, uYear, uColor, uModel;
 
-	car userCar;
-	cout << "What would you like to do?" << endl;
-	cout << "Choose Year (y)" << endl << "Choose Make (m)" << endl << "Choose Model (o)" << endl << "Choose Color (c)" << endl <<
-		"Start Car (s)" << endl << "Stop Car (t)" << endl << "Print your car (p)" << endl;
+	carAtts userCar;
+	
 
-	cin >> option;
+	while (keepGoing == true) {
 
-	userCar.selectAtt(option);
+		cout << "What would you like to do?" << endl;
+		cout << "1 = Choose Year" << endl << "2 = Choose Make" << endl << "3 = Choose Model" << endl << "4 = Choose Color" << endl <<
+			"5 = Start Car" << endl << "6 = Stop Car" << endl << "7 = Print your car" << endl;
 
-	cin >> stay;
+		cin >> option;
+
+
+		switch (option) {
+		case(1): {
+			cout << "Choose the year of your car: ";
+			cin.ignore();
+			getline(cin, uYear);
+			userCar.setYear(uYear);
+			break; }
+		case(2): {
+			cout << "Choose the make of your car: ";
+			cin.ignore();
+			getline(cin, uMake);
+			userCar.setMake(uMake);
+			break;
+		}
+		case(3): {
+			cout << "Choose the model of your car: ";
+				cin.ignore();
+			getline(cin, uModel);
+			userCar.setModel(uModel);
+			break;
+		}
+		case(4): {
+			cout << "Choose the color of your car: ";
+			cin.ignore();
+			getline(cin, uColor);
+			userCar.setColor(uColor);
+			break;
+		}
+		case(5): {
+			userCar.startCar();
+			break;
+		}
+		case(6): {
+			userCar.stopCar();
+			break;
+		}
+		case(7): {
+			userCar.printCar();
+			break;
+		}
+		}
+		cout << endl;
+	}
+	
 	return 0;
 }
